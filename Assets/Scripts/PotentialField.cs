825a19289c14c3aa2198400575e9f5b7bc2db4ae
Daniel Angres <gdnlclub@gmail.com>
2022-04-02 13:37:25 -0700

    Added Potential Field basics, placeholder AIMgr

---
+++ Assets/Scripts/PotentialField.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotentialField : MonoBehaviour
+{
+    public float c;
+    public float e;
+    public string obstacleType;
+
+    public float boulderC = 1.5f;       // TODO Adjust these values!
+    public float boulderE = -2f;
+    public float robotC = 1.5f;
+    public float robotE = -2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (obstacleType) {
+            case "robot":
+                c = robotC;
+                e = robotE;
+                break;
+            case "boulder":
+                c = boulderC;
+                e = boulderE;
+                break;
+            default:
+                c = 1.5f;
+                e = -2f;
+                break;
+        }
+
+        // Optional code, scales attractive/repulsive force by entity scale. Will enable if needed
+        // c *= Mathf.Max(this.gameObject.transform.localScale.x, this.gameObject.transform.localScale.y);
+    }
+
+    public float GetForceScalar(Vector3 b, Vector3 a) {
+        if (a == b) {
+            Debug.Log("got 0 scalar");
+        }
+        return Mathf.Pow((a - b).magnitude, e) * c;
+    }
+
+    public Vector3 GetForceVector(Vector3 a, Vector3 b) {
+        Vector3 uni = (a - b);
+        uni.Normalize();
+        return GetForceScalar(a, b) * uni;
+    }
+}
