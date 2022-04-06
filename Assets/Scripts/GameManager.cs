c9cf9574416e860ee8039a5282fe73c53b107ad2
Daniel Angres <gdnlclub@gmail.com>
2022-04-06 13:26:36 -0700

    Added game-over mechanics upon player 'death', game over screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
 
     public static GameManager inst;
     public PotentialField player;
+    public bool gameOver;
+    public Canvas gameOverScreen;
 
     void Awake() {
         inst = this;
@@ -14,6 +16,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameOverScreen.gameObject.SetActive(false);
         inst = this;
     }
 
@@ -21,5 +24,17 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void EndGame() {
+        gameOver = true;
+    }
+
+    public void GameOver() {
+        EndGame();
+        // Make game over screen visible
+        gameOverScreen.gameObject.SetActive(true);
+        // Destroy everybody
+        EntityMgr.inst.ShutDownEntityGen();
     }
 }
